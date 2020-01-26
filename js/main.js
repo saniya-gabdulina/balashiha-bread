f9a0e84e18bfc6bb2161b13def48e3840afbc93a
saniya.gabdulina <saniya.gabdulina@gmail.com>
2020-01-27 01:43:28 +0600

    add all sections

--- js/main.js
+++ js/main.js
@@ -20,4 +20,19 @@ $(document).ready(function () {
     next.css('left', prev.width() + 10 + bulllets.width() +10)
     bulllets.css('left', prev.width() + 10)
 
+
+    var isAddedMap = false;
+
+    $(window).scroll(function() {
+        var el = $('.map');
+        if ($(this).scrollTop() > el.offset().top - 800) {
+            if(isAddedMap) return;
+            isAddedMap = true;
+            var script = document.createElement('script');
+            script.src = "https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A3640a6472e0817484e41c82f3bab31623c39b2a8835bc6ba7b8029c7367f5a4f&amp;width=100%25&amp;height=100%&amp;lang=ru_RU&amp;scroll=false";
+            el.append(script);
+        };
+    });
+
+    
 });
